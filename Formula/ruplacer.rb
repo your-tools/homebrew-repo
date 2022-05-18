2806b8a09250b22e63563acba73d9bc1af7c08d6
maximerety <maxime.rety@gmail.com>
2022-05-18 12:08:17 +0200

    ruplacer 0.8.0

--- Formula/ruplacer.rb
+++ Formula/ruplacer.rb
@@ -1,8 +1,8 @@
 class Ruplacer < Formula
   desc "Find and replace text in source files"
   homepage "https://github.com/dmerejkowsky/ruplacer/"
-  url "https://github.com/dmerejkowsky/ruplacer/archive/v0.6.4.tar.gz"
-  sha256 "4f66e8970942e55dc287c585eef7a21394aefa49df746cef429f9e5bc6714c7a"
+  url "https://github.com/dmerejkowsky/ruplacer/archive/v0.8.0.tar.gz"
+  sha256 "7109bf19c0f62ec477c952c1c509353f7c6abd19deb9169384ebc9d21ad71dc7"
   head "https://github.com/dmerejkowsky/ruplacer"
 
   depends_on "rust" => :build
